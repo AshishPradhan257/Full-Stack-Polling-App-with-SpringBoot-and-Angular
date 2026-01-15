1e0ef36161492056818549bf9cfa1cdbabda1d23
AshishPradhan257 <ashish257pradhan@gmail.com>
2026-01-15 23:28:19 +0530

    creating all the tables and connecting each layer to the other

---
+++ votingapp/src/main/java/com/voting/votingapp/model/Poll.java
@@ -0,0 +1,26 @@
+package com.voting.votingapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String question;
+
+    @ElementCollection //jpa will create a separate table for this! by using this annotation
+    private List<OptionVote> options = new ArrayList<>();
+
+//    @ElementCollection
+//    private List<Long> votes = new ArrayList<>();
+
+}
