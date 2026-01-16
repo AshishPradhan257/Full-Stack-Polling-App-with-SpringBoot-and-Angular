026fe1785e7b8d3a017155a7e06e7fadfc45bc52
AshishPradhan257 <ashish257pradhan@gmail.com>
2026-01-16 23:41:05 +0530

    get polls api

--- votingapp/src/main/java/com/voting/votingapp/controllers/PollController.java
+++ votingapp/src/main/java/com/voting/votingapp/controllers/PollController.java
@@ -3,10 +3,9 @@ package com.voting.votingapp.controllers;
 
 import com.voting.votingapp.model.Poll;
 import com.voting.votingapp.services.PollService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/polls")
@@ -22,6 +21,12 @@ public class PollController {
 
     @PostMapping
     public Poll createPoll(@RequestBody Poll poll){
+
         return pollService.createPoll(poll);
+    }
+
+    @GetMapping
+    public List<Poll> getAllPolls(){
+        return pollService.getAllPolls();
     }
 }
--- votingapp/src/main/java/com/voting/votingapp/services/PollService.java
+++ votingapp/src/main/java/com/voting/votingapp/services/PollService.java
@@ -4,16 +4,24 @@ import com.voting.votingapp.model.Poll;
 import org.springframework.stereotype.Service;
 import com.voting.votingapp.repositories.PollRepository;
 
+import java.util.List;
+
 @Service
 public class PollService {
 
     private final PollRepository pollRepository;
 
     public PollService(PollRepository pollRepository) {
+
         this.pollRepository = pollRepository;
     }
 
     public Poll createPoll(Poll poll) {
+
         return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
     }
 }
